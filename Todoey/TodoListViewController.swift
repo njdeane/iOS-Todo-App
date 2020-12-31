13d5a4fb827adcce06b7f3ebb9966ef9f1910deb
njdeane <njdeane7@gmail.com>
2020-12-31 15:56:20 +1000

    Create itemArray to populate cells, reusable cell created to populate rows

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    //Creates the number of rows by determining and returning the number of items in the itemArray
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //Creates a reusable cell, populates each cell with items from itemArray
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
 
 }
 
