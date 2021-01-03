da3188638b4b958065151f137828411d39d59757
njdeane <njdeane7@gmail.com>
2021-01-03 18:06:39 +1000

    Add searchBar dismiss on main thread in searchBar func.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -122,6 +122,10 @@ extension TodoListViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()                
+            }
         }
     }
 }
